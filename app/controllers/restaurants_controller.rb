117395ef77df6566b98519278c5e4818680a0cb2
Lara Franchi <laradeandrade@protonmail.com>
2023-02-22 16:19:01 -0500

    RailsYelp

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -7,6 +7,14 @@ class RestaurantsController < ApplicationController
   def show
   end
 
-  
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    redirect_to restaurant_path(@restaurant)
+  end
 
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,14 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
+  get '/restaurants', to: 'restaurants#index'
+
   get '/restaurants/new', to: 'restaurants#new', as: :new_restaurant
 
   post '/restaurants', to: 'restaurants#create'
 
   # See details about one restaurant
   get '/restaurants/:id', to: 'restaurants#show', as: :restaurant
+
+  get '/reviews', to: 'reviews#new'
 end
