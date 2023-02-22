dd12f00dea32f889e50bc482aa5ffbb2c37c0965
Lara Franchi <laradeandrade@protonmail.com>
2023-02-22 15:13:42 -0500

    RailsYelp

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,6 @@
 class Restaurant < ApplicationRecord
+  has_many :reviews
+
   CATEGORY = %w(chinese italian japanese french belgian)
   validates :name, :address, :category, presence: true
   validates :category, inclusion: { in: CATEGORY }
