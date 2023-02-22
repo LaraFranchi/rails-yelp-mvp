9916f04f6de3b357ddad9ca19a1e53fb7fe88700
Lara Franchi <laradeandrade@protonmail.com>
2023-02-22 15:04:07 -0500

    RailsYelp

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,5 @@
 class Restaurant < ApplicationRecord
-  CATEGORIES = %w(chinese italian japonese french belgian)
-  validates :name, address, categories, presence: true
-  validates :categories, inclusion { in: CATEGORIES }
-
-
+  CATEGORY = %w(chinese italian japanese french belgian)
+  validates :name, :address, :category, presence: true
+  validates :category, inclusion: { in: CATEGORY }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get '/restaurants/new', to: 'restaurants#new', as: :new_restaurant
+
+  post '/restaurants', to: 'restaurants#create'
+
+  # See details about one restaurant
+  get '/restaurants/:id', to: 'restaurants#show', as: :restaurant
 end
